27d0160c996d2cc1d599971de0a9460dddbab917
Piyush Sahu <piyushra71@gmail.com>
2025-11-06 14:26:00 +0530

    Notifications

--- backend/PlacementCoordinator/models.py
+++ backend/PlacementCoordinator/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+# from django.contrib.auth.models import User
 
 User = get_user_model()
 
@@ -58,3 +59,14 @@ class JobApplication(models.Model):
 
     def __str__(self):
         return f"{self.student.username} - {self.job.job_id} ({self.status})"
+
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notifications")
+    title = models.CharField(max_length=255)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} -> {self.recipient.username}"
--- backend/PlacementCoordinator/urls.py
+++ backend/PlacementCoordinator/urls.py
@@ -1,7 +1,15 @@
 # job/urls.py
 from django.urls import path
 from .views import (
-    JobPostingListCreateAPIView, JobPostingDetailAPIView, JobApplicationAPIView,MyJobApplicationsAPIView,CompanySummaryView,CompanyDetailsView
+    JobPostingListCreateAPIView,
+    JobPostingDetailAPIView,
+    JobApplicationAPIView,
+    MyJobApplicationsAPIView,
+    CompanySummaryView,
+    CompanyDetailsView,
+    NotificationListView,
+    MarkNotificationReadView,
+    SendCustomNotificationView
     
 )
 
@@ -16,7 +24,8 @@ urlpatterns = [
          name="my-applications"),
      path("companies-summary/", CompanySummaryView.as_view(), name="company-summary"),
      path("company-details/<str:company_name>/",CompanyDetailsView.as_view(), name="company-details"),
-
-
+      path('notifications/', NotificationListView.as_view(), name='notifications'),
+    path('notifications/<int:pk>/mark-read/', MarkNotificationReadView.as_view(), name='mark-notification-read'),
+    path('send-notification/', SendCustomNotificationView.as_view(), name='send-notification'),
 
 ]
